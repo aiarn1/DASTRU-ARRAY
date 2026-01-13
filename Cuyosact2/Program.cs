4d9a94ff7c8684368311d23962e303640a57764a
Aian Cuyos <aiarn1006@gmail.com>
2026-01-13 17:10:26 +0800

    Add files via upload

---
+++ Cuyosact2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyosact3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Create an Array of Sports Objects
+            Sports[] sportsArray = new Sports[4];
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Populate the Array with Basketball and Objects
+            sportsArray[0] = new Basketball(" COLLEGE OF COMPUTER STUDIES", "Louis Aian Cuyos", "24-2002943", 43);
+            sportsArray[1] = new Basketball(" COLLEGE OF ARTS AND PEDEALOGY", "Ryan Carl Salvo", "24-2002227", 13);
+
+
+            // Populate the Array with Arnis Objects
+            sportsArray[2] = new Arnis(" COLLEGE OF BUSINESS MANAGEMENT", "Carls Kevin Belleza", "22-9002562", 15);
+            sportsArray[3] = new Arnis(" COLLEGE OF NURSING", "John Rhaezel Atillo", "22-9002562", 20);
+
+
+            // Looping Stucture to display information
+            foreach (Sports sport in sportsArray)
+            {
+                // Display Information
+                sport.DisplayInfo();
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ Cuyosact2/Sports.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyosact3
+{
+    class Sports
+    {
+        // Private Fields 
+        private string TeamName { get; set; }
+        private string PlayerName { get; set; }
+        private string TeamID { get; set; }
+
+        // Parameterized Constructor
+        public Sports(string teamName, string name, string teamID)
+        {
+            TeamName = teamName;
+            PlayerName = name;
+            TeamID = teamID;
+        }
+        // Method to display information
+        public virtual void DisplayInfo()
+        {
+
+            Console.WriteLine($"Team Name    :{TeamName} ");
+            Console.WriteLine($"Player Name  : {PlayerName}");
+            Console.WriteLine($"Team ID      : {TeamID}");
+        }
+    }
+
+    // Derived Class 1
+    class Basketball : Sports
+    {
+        // Private Field
+        private double PointsScored { get; set; }
+
+        // Parameterized Constructor
+        public Basketball(string teamName, string name, string teamID, int pointsScored)
+            : base(teamName, name, teamID)
+        {
+            PointsScored = pointsScored;
+        }
+        // Override DisplayInfo method
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n----- Basketball Player Info -----");
+            base.DisplayInfo();
+            Console.WriteLine($"Points Scored: {PointsScored}\n");
+
+        }
+    }
+    // Derived Class 2
+    class Arnis : Sports
+    {
+        // Private Field
+        private int MatchesWon { get; set; }
+
+        // Parameterized Constructor
+        public Arnis(string teamName, string name, string teamID, int matchesWon)
+            : base(teamName, name, teamID)
+        {
+            MatchesWon = matchesWon;
+        }
+        // Override DisplayInfo method
+        public override void DisplayInfo()
+        {
+
+            Console.WriteLine("\n***** Arnis Player Info *****");
+            base.DisplayInfo();
+            Console.WriteLine($"Matches Won  : {MatchesWon}\n");
+
+        }
+    }
+}
