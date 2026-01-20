853ead1353a1fa8ea85a7d01d66b25910c561cb0
Aian Cuyos <aiarn1006@gmail.com>
2026-01-20 17:13:34 +0800

    Refactor athlete registration method for clarity
    Updated athlete registration method to include specific input for Basketball and Arnis.

--- Act5_User input method/Program.cs
+++ Act5_User input method/Program.cs
@@ -29,39 +29,42 @@ namespace Cuyos_act5
         }
         // Method to register athletes
         static Sports[,] RegisterAthletes()
-        {
-            // Create a 2D array to hold sports objects
-            Sports[,] sportsArray = new Sports[2, 3];
-            Console.ForegroundColor = ConsoleColor.Yellow;
+ {
+     // Create a 2D array to hold Sports objects
+     Sports[,] sportsArray = new Sports[2, 3];
+     Console.ForegroundColor = ConsoleColor.Yellow;
+     // Loop to gather user input for each athlete
+     for (int i = 0; i < sportsArray.GetLength(0); i++)
+     {
+         // Prompt user to choose sport
+         Console.Write("\n\tChoose Sport: [1] Basketball [2] Arnis: ");
+         string choice = Console.ReadLine();
+         Console.WriteLine("\n\tSelection: " + (choice == "1" ? "Basketball" : "Arnis"));
+         // Inner loop to gather details for each athlete
+         for (int j = 0; j < sportsArray.GetLength(1); j++)
+         {
+             // Choose Department
+             Console.WriteLine("\tAvailable Department --- CCS, CBM, CN, CASP ---");
+             Console.Write("\tDept: "); string dept = Console.ReadLine();
+             Console.Write("\tName: "); string name = Console.ReadLine();
+             Console.Write("\tID  : "); string id = Console.ReadLine();
+             if (choice == "1") // Basketball
+             {
+                 Console.Write("\tPts : ");
+                 int pts = int.Parse(Console.ReadLine());
+                 sportsArray[i, j] = new Basketball(dept, name, id, pts);
+             }
+             else // Arnis
+             {
+                 Console.Write("\tMatches Won : ");
+                 int matchesWon = int.Parse(Console.ReadLine());
+                 sportsArray[i, j] = new Arnis(dept, name, id, matchesWon);
+             }
+             Console.WriteLine("\tAdded successfully.\n");
+         }
+     }
+     return sportsArray;
+ }
 
-            // Input loop for athlete registration
-            for (int i = 0; i < sportsArray.GetLength(0); i++)
-            {
-                Console.Write("\n\tChoose Sport: [1] Basketball [2] Arnis: ");
-                string choice = Console.ReadLine();
-                Console.WriteLine("\n\tSelection: " + (choice == "1" ? "Basketball" : "Arnis"));
-                
-                // Inner loop for each athlete in the sport
-                for (int j = 0; j < sportsArray.GetLength(1); j++)
-                {
-                    // Display department options
-                    Console.WriteLine("\tAvailable Department --- CCS, CBM, CN, CASP ---");
-                    // Input athlete details
-                    Console.Write("\tDept: "); string dept = Console.ReadLine();
-                    Console.Write("\tName: "); string name = Console.ReadLine();
-                    Console.Write("\tID  : "); string id = Console.ReadLine();
-                    Console.Write("\tPts : "); int pts = int.Parse(Console.ReadLine());
-                    
-                    // Create appropriate object based on sport choice
-                    if (choice == "1")
-                        sportsArray[i, j] = new Basketball(dept, name, id, pts);
-                    else
-                        sportsArray[i, j] = new Arnis(dept, name, id, pts);
 
-                    Console.WriteLine("\tAdded successfully.\n");
-                }
-            }
-            return sportsArray;
-        }
-    }
-}
+
